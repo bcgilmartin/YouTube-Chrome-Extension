affcde318d8c037e0eedcc890bee96a0d9150e53
bcgilmartin <blakegilmartin96@gmail.com>
2017-12-26 21:41:08 -0800

    Now gets email. -Blake

--- content.js
+++ content.js
@@ -1,16 +1,35 @@
 var oldVidTitle = "";
+var newVidTitle = "";
+var oldEmail = "";
+var newEmail = "";
 
 //runs checkURLChange every 500 ms to catch YouTube title change
 //Note: YouTube does not load a new URL it only updates it so this is needed
-var titleChangeHandler = window.setInterval(checkTitleChange, 500)
+var titleChangeHandler = window.setInterval(checkTitleChange, 500);
+
+// var clientLoadHandler = handleClientLoad();
 
 //Gets new video title and if it is different from the old one it will run
 //the rest of the fucntion
 //GETS: URL, Title
 function checkTitleChange(){
 
+  if(newEmail != oldEmail || oldEmail == "") {
+    document.getElementById("avatar-btn").click();
+    document.getElementById("avatar-btn").click();
+
+    try{
+      oldEmail = document.getElementById("email").innerText;
+      newEmail = oldEmail;
+    }
+    catch(err){}
+  }
+
   //Get new video title
-  newVidTitle = document.getElementsByTagName("h1")[0].innerText;
+  try {
+    newVidTitle = document.getElementsByTagName("h1")[0].innerText;
+  }
+  catch(err){}
 
   //check with old video
   if(oldVidTitle !== newVidTitle){
@@ -21,10 +40,24 @@ function checkTitleChange(){
     //set old VidTitle to new
     oldVidTitle = newVidTitle;
 
-    //hi
+    document.getElementById("avatar-btn").click();
+    document.getElementById("avatar-btn").click();
+
     //Prints to console for now
     //Will send to database here
     console.log(newVidTitle);
     console.log(currentURL);
+    console.log(oldEmail);
+    objectToSend = {email:oldEmail, url:currentURL, vidTitle:newVidTitle};
   }
+
+  // try{
+  //   newEmail = document.getElementById("email").innerText;
+  // }
+  // catch(err){}
+  //
+  // if(oldEmail !== newEmail || oldEmail == ""){
+  //   oldEmail = newEmail;
+  // }
+
 }
