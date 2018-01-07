253b6a786de02c2ae851c0602a5a38813e09aa02
EC2 Default User <ec2-user@ip-172-31-27-173.us-east-2.compute.internal>
2018-01-07 14:37:10 +0000

    unable to access rds

--- RDS/MySQLDatabaseConnection.js
+++ RDS/MySQLDatabaseConnection.js
@@ -1,11 +1,29 @@
 var mysql = require('mysql');
-var AuthKeysRDS = ('./AuthKeysRDS');
+var filePath = ('./AuthKeysRDS');
+var AuthKeysRDS = JSON.parse(filePath);
 
 var connection = mysql.createConnection({
 	host	 : AuthKeysRDS.host,
 	user     : AuthKeysRDS.user,
 	password : AuthKeysRDS.password,
-	port	 : AuthKeysRDS.port
+	port	 : AuthKeysRDS.port,
+	database : AuthKeysRDS.database
+});
+
+console.log({
+	host	 : AuthKeysRDS.host,
+	user     : AuthKeysRDS.user,
+	password : AuthKeysRDS.password,
+	port	 : AuthKeysRDS.port,
+	database : AuthKeysRDS.database
+
+});
+
+console.log({
+    host            : process.env.MYSQL_HOST,
+    user            : process.env.MYSQL_USER,
+    password        : process.env.MYSQL_SECRET,
+    database        : process.env.MYSQL_DB
 });
 
 connection.connect(function(err){
